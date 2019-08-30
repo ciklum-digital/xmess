73acaa52fd7a2746b761c744c4268aa56ba66c06
Denis Vada <dvad@ciklum.com>
2019-08-30 21:54:18 +0300

    refactor: fix sonar issues

--- packages/core/src/modules/xmess/classes/hook.class.ts
+++ packages/core/src/modules/xmess/classes/hook.class.ts
@@ -1,7 +1,7 @@
 import { IHook, IHookSubscriber } from '../interfaces/hook.interface';
 
 export class Hook implements IHook {
-  private subscribers: IHookSubscriber[] = [];
+  private readonly subscribers: IHookSubscriber[] = [];
 
   public call(...props: any[]): void {
     this.subscribers.forEach((subscriber: IHookSubscriber) => subscriber(...props));
